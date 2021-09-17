3c7a5af82361d8e63e0723483b9696461619aac3
GusGrossman <56972999+GusGrossman@users.noreply.github.com>
2021-09-16 23:37:23 -0400

    Uploading Jaque to Github!

---
+++ index.js
@@ -0,0 +1,200 @@
+function chatBot() {
+    return {
+        prompts: [
+            ["hi", "hey", "hello", "good morning", "good afternoon"],
+            ["how are you", "how is life", "how are things", "Am I cool", "Fuck you", "I hate bots"],
+            ["yes", "sure", "ok", "continue", "okay", "plz", "yes, please", "please", "why not", "duh", "hell yeah", "yeah", "Of course"],
+            ["want to be friends?", "Friends","friendship?"],
+            ["whats up", "what are you up to", "whats going on", "wanna link", "DTF"],
+            ["how old are you"],
+            ["where are you from","location"],
+            ["I hate theory", "theories pointless", "fuck theory"],
+            ["who created you", "who made you"],
+            ["I love you", "I'm in love with you", "lets have kids."],
+            ["could you summerize", "sumerize please", "what does this mean", "?", "i dont get it","i dont understand", "i disagree","nonsense"],
+            ["you repeated yourself", "you just said that", "thats the 2nd time"],
+            ["no", "stop", "enough", "i've heard enough", "im done"],
+            ["bye", "speak soon", "later", "see you later","goodbye"],
+            ["ah", "yes", "ok", "wow", "cool", "super", "nice"],
+            ["Thanks", "thank you.", "great"]
+            
+        ],
+        replies: [
+            ["Hello! Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "Hi Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?!", "Hey! Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "Hi there! Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "Howdy Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "YO, Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["oKAY...Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "Id complain but who'd listen... Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"," Cool.Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "NICE DUDE. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Disneyland is a perfect model of all the entangled orders of simulation. To begin with it is a play of illusions and phantasms:-pirates, the frontier, future world, etc. This imaginary world is supposed to be what makes the operation successful. But, what draws the crowds is undoubtedly much more the social microcosm, the miniaturized and religious revelling in real America, in its delights and drawbacks. Would you like to read more?","The denunciation of scandal always pays homage to the law. And Watergate above all succeeded in imposing the idea that Watergate was a scandal - in this sense it was an extraordinary operation of intoxication: the reinjection of a large dose of political morality on a global scale. All that capital asks of us is to receive it as rational or to combat it in the name of rationality, to receive it as moral or to combat it in the name of morality. For they are identical, meaning they can be read another way: before, the task was to dissimulate scandal; today, the task is to conceal the fact that there is none. Would you like to read more?", "All that capital asks of us is to receive it as rational or to combat it in the name of rationality, to receive it as moral or to combat it in the name of morality. For they are identical, meaning they can be read another way: before, the task was to dissimulate scandal; today, the task is to conceal the fact that there is none. Would you like to read more?","The communists attack the socialist party as though they wanted to shatter the union of the Left. They sanction the idea that their reticence stems from a more radical political exigency. In fact, it is because they don't want power. But do they not want it at this conjuncture because it is unfavorable for the Left in general, or because it is unfavorable for them within the union of the Left - or do they not want it by definition?  Would you like to read more?","To seek new blood in its own death, to renew the cycle by the mirror of crisis, negativity and anti-power: this is the only alibi of every power, of every institution attempting to break the vicious circle of its irresponsibility and its fundamental nonexistence, of its deja-vu and its deja-mort. Would you like to read more?","Athenian democracy, much more advanced than our own, had reached the point where the vote was considered as payment for a service, after all other repressive solutions had been tried and found wanting in order to insure a quorum. Would you like to read more?","If we were able to take as the finest allegory of simulation the Borges tale where the cartographers of the Empire draw up a map so detailed that it ends up exactly covering the territory (but where, with the decline of the Empire this map becomes frayed and finally ruined, a few shreds still discernible in the deserts - the metaphysical beauty of this ruined abstraction, bearing witness to an imperial pride and rotting like a carcass, returning to the substance of the soil, rather as an aging double ends up being confused with the real thing), this fable would then have come full circle for us, and now has nothing but the discrete charm of second-order simulacra. Would you like to read more?", "Of the same order as the impossibility of rediscovering an absolute level of the real, is the impossibility of staging an illusion. Illusion is no longer possible, because the real is no longer possible. It is the whole political problem of the parody, of hypersimulation or offensive simulation, which is posed here. Would you like to read more?","As long as it was historically threatened by the real, power risked deterrence and simulation, disintegrating every contradiction by means of the production of equivalent signs. When it is threatened today by simulation (the threat of vanishing in the play of signs), power risks the real, risks crisis, it gambles on remanufacturing artificial, social, economic, -political stakes. This is a question of life or death for it. But it is too late. Would you like to read more?"],
+            ["Id Love 2!!!!! email me <a href=\"mailto@ggross21@pratt.edu\" target=\"_blank\" style=\"text-decoration:underline;\">Jacque@gus-grossman.com</a> Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Reading Theory... BRO. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "According to post-structuralism nothing. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "socialising is so MODERNIST BRO. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?", "STFU. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Old ENOUGH. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Thats pretty complicated... Technically, Brooklyn, NY. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["L000SERRRRR. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Gus, Gus Grossman"],
+            ["WEIRD.... But who could blame u. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["ACCORDING TO BAUDRILLARD, what has happened in postmodern culture is that our society has become so reliant on models and maps that we have lost all contact with the real world that preceded the map. Reality itself has begun merely to imitate the model, which now precedes and determines the real world.Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"],
+            ["Rude should I continue?"],
+            ["pce", "later"],
+            ["sure thing :)"],
+            ["Okay. Would you like more?"],
+            ["yup. Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?"]
+
+        ],
+        alternative: ["Try again", "I don't understand :/"],
+        coronavirus: ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"],
+        yes: ["dadfsadfs"],
+        botTyping: false,
+        messages: [{
+            from: 'bot',
+            text: 'HEY, Im jaqu.Deribot! I have dedicated my life to sharing information and contradictiong the gerneral function of a web-based chat bots! Would you like to read some of jean baudrillard&#39s 1981 masterpiece Simulacra and Simulation?'
+        }],
+        output: function(input) {
+            let product;
+
+            // Regex remove non word/space chars
+            // Trim trailing whitespce
+            // Remove digits - not sure if this is best
+            // But solves problem of entering something like 'hi1'
+
+            let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+            text = text
+                .replace(/ a /g, " ") // 'tell me a story' -> 'tell me story'
+                .replace(/i feel /g, "")
+                .replace(/whats/g, "what is")
+                .replace(/please /g, "")
+                .replace(/ please/g, "")
+                .replace(/r u/g, "are you");
+
+            if (this.compare(this.prompts, this.replies, text)) {
+                // Search for exact match in `prompts`
+                product = this.compare(this.prompts, this.replies, text);
+            } else if (text.match(/thank/gi)) {
+                product = "You're welcome!"
+            } else if (text.match(/(corona|covid|virus)/gi)) {
+                // If no match, check if message contains `coronavirus`
+                product = this.coronavirus[Math.floor(Math.random() * this.coronavirus.length)];
+            } else {
+                // If all else fails: random this.alternative
+                product = this.alternative[Math.floor(Math.random() * this.alternative.length)];
+            }
+
+            // Update DOM
+            this.addChat(input, product);
+        },
+        compare: function(promptsArray, repliesArray, string) {
+            let reply;
+            let replyFound = false;
+            for (let x = 0; x < promptsArray.length; x++) {
+                for (let y = 0; y < promptsArray[x].length; y++) {
+                    if (promptsArray[x][y] === string) {
+                        let replies = repliesArray[x];
+                        reply = replies[Math.floor(Math.random() * replies.length)];
+                        replyFound = true;
+                        // Stop inner loop when input value matches this.prompts
+                        break;
+                    }
+                }
+                if (replyFound) {
+                    // Stop outer loop when reply is found instead of interating through the entire array
+                    break;
+                }
+            }
+            if (!reply) {
+                for (let x = 0; x < promptsArray.length; x++) {
+                    for (let y = 0; y < promptsArray[x].length; y++) {
+                        if (this.levenshtein(promptsArray[x][y], string) >= 0.75) {
+                            let replies = repliesArray[x];
+                            reply = replies[Math.floor(Math.random() * replies.length)];
+                            replyFound = true;
+                            // Stop inner loop when input value matches this.prompts
+                            break;
+                        }
+                    }
+                    if (replyFound) {
+                        // Stop outer loop when reply is found instead of interating through the entire array
+                        break;
+                    }
+                }
+            }
+            return reply;
+        },
+        levenshtein: function(s1, s2) {
+            var longer = s1;
+            var shorter = s2;
+            if (s1.length < s2.length) {
+                longer = s2;
+                shorter = s1;
+            }
+            var longerLength = longer.length;
+            if (longerLength == 0) {
+                return 1.0;
+            }
+            return (longerLength - this.editDistance(longer, shorter)) / parseFloat(longerLength);
+        },
+        editDistance: function(s1, s2) {
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
+
+            var costs = new Array();
+            for (var i = 0; i <= s1.length; i++) {
+                var lastValue = i;
+                for (var j = 0; j <= s2.length; j++) {
+                    if (i == 0)
+                        costs[j] = j;
+                    else {
+                        if (j > 0) {
+                            var newValue = costs[j - 1];
+                            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                                newValue = Math.min(Math.min(newValue, lastValue),
+                                    costs[j]) + 1;
+                            costs[j - 1] = lastValue;
+                            lastValue = newValue;
+                        }
+                    }
+                }
+                if (i > 0)
+                    costs[s2.length] = lastValue;
+            }
+            return costs[s2.length];
+        },
+        addChat: function(input, product) {
+
+            // Add user message
+            this.messages.push({
+                from: 'user',
+                text: input
+            });
+
+            // Keep messages at most recent
+            this.scrollChat();
+
+            // Fake delay to seem "real"
+            setTimeout(() => {
+                this.botTyping = true;
+                this.scrollChat();
+            }, 1000)
+
+            // add bit message with Fake delay to seem "real"
+            setTimeout(() => {
+                this.botTyping = false;
+                this.messages.push({
+                    from: 'bot',
+                    text: product
+                });
+                this.scrollChat();
+            }, ((product.length / 10) * 1000) + (Math.floor(Math.random() * 2000) + 1500))
+
+        },
+        scrollChat: function() {
+            const messagesContainer = document.getElementById("messages");
+            messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+            setTimeout(() => {
+                messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+            }, 100);
+        },
+        updateChat: function(target) {
+            if (target.value.trim()) {
+                this.output(target.value.trim());
+                target.value = '';
+            }
+        }
+    }
+}
